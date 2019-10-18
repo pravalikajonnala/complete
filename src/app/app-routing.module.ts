b4df2c15a1aafb85e9b92ec49d2117d47bd34658
Sreerama Konduri <sreerama.konduri@trackercorp.com>
2019-10-18 11:42:48 +0530

    branch1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Branch1Component } from './branch1/branch1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'branch1',component:Branch1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Branch1Component } from './branch1/branch1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Branch1Component
   ],
   imports: [
     BrowserModule,
